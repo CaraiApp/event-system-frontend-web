bbb305adac1bf644a12535c4e6e7fe4352b9cc4e
caraiapp <info@caraimelilla.com>
2025-03-14 09:54:49 +0100

    Añadir archivo de strings con caracteres acentuados correctamente codificados

---
+++ src/utils/strings.js
@@ -0,0 +1,10 @@
+export const COMMON_STRINGS = {
+  configuracion: "Configuración",
+  cerrarSesion: "Cerrar Sesión",
+  analiticas: "Analíticas",
+  ocupacion: "Ocupación",
+  proximos: "Próximos",
+  funcion: "Función",
+  metodo: "Método",
+  euro: "€"
+};
